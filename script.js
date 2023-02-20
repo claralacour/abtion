a70c3da679549a9902b9c061988efe69ddf1beac
claralacour <denlacour@gmail.com>
2023-02-20 13:03:26 +0100

    burgermenu

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+
+//js til burgermenu
+const burgermenu = document.querySelector(".burgermenu");
+const navList = document.querySelector(".navlist");
+
+burgermenu.addEventListener("click", () =>{
+  burgermenu.classList.toggle("active");
+  navList.classList.toggle("active");
+})
+
+document.querySelectorAll(".navlink").forEach(n => n. addEventListener("click",() => {
+  burgermenu.classList.remove("active");
+  navList.classList.remove("active");
+}))
