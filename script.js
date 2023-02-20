e214dd6653768e88555bbad55608aae7d1f70578
Jonas Dahlstrøm <119883850+JODMedia@users.noreply.github.com>
2023-02-20 10:17:15 +0100

    HTML CSS skelet + navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+//Sticky header til navbar
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
