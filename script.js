d7b9d7706e227700301ca77f9a6357a70d3e33c9
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-02-21 12:06:34 +0100

    Tilføjet basale funktioner

--- script.js
+++ script.js
@@ -18,3 +18,38 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// Price calculator
+
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2");
+
+const radioWebsite = document.getElementById("radio-website");
+const radioApp = document.getElementById("radio-app");
+const radioSystem = document.getElementById("radio-system");
+
+form1.addEventListener("change", function(){
+  if (radioWebsite.checked){
+    form2.innerHTML = "";
+    var form2Array = ["new-website|Opbygning af ny website", "webshop|Webshop til eksisterende hjemmeside", "redesign-website|Redesign af eksisterende website", "maintain-website|Vedligeholdelse"];
+  } else if (radioApp.checked){
+    form2.innerHTML = "app"
+  } else if (radioSystem.checked){
+    form2.innerHTML = "system"
+  } 
+
+  for (let i in form2Array){
+    let pair = form2Array[i].split("|");
+    let newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.id = pair[0];
+    newCheckbox.innerHTML = pair[1];
+    let label = document.createElement("label");
+    label.htmlFor = pair[0];
+    label.innerHTML = pair[1] + "<br>";
+    form2.appendChild(newCheckbox);
+    form2.appendChild(label);
+  }
+});
+
