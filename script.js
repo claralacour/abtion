24cf306695b4afe1079cad700e30977403faa72c
Jonas Dahlstrøm <jod.marketing.bureau@gmail.com>
2023-02-21 11:20:10 +0100

    Preloader opdatering med kommentarer

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 //Preloader script
-//preloadingFade fader baggrunden og teksten ud
-function preloadingFade() {
-  const preloadingBg = document.querySelector(".preloader-bg");
-  const preloadingParagraph = document.querySelector(".preloader-text");
-  preloadingBg.style.opacity = "0";
-  preloadingParagraph.style.opacity = "0";
-}
+//Variablerne bliver navngivet
+let preload = document.querySelector(".preloader-bg");
+let preloadContainer = document.querySelector(".preloader-text-container");
+let preloadParagraph = document.querySelectorAll(".preloader-text");
 
-//preloadingRemove fjerner det udsynlige div element, hvilket gør at man kan klikke på links på siden når loadingRemove bliver kaldt.
-function preloadingRemove() {
-  const preloading = document.querySelector(".preloader");
-  preloading.style.display = "none";
-}
+//addEventListener lytter efter DOMContentLoaded, for at sikre sig at funktionen kører når DOM er loadet
+window.addEventListener("DOMContentLoaded", () => {
+  //setTimeout sætter en timer på en funktion, => er en "arrow function", hvilket er et kompakt alternativ til en normal funktion
+  setTimeout(() => {
+    //forEach loader her hver paragraph element, og dens index nummer (0, 1 og 2). Her tilføjer p.classList.add "active" classen fra style.cs.
+    //(idx + 1) * 140) beskriver at funktionen tager paragraphens index i rækkefølge, og derefter plusser med 1 hvert 140'ende millisekund.
+    //Dette gør at hvert paragraph element får "active" classen med 140 millisekunders mellemrum, og giver den ønsket animations-effekt.
+    preloadParagraph.forEach((p, idx) => {
+      setTimeout(() => {
+        p.classList.add("active");
+      }, (idx + 1) * 140);
+    });
 
-function preloader() {
-  window.setInterval(preloadingFade, 1000);
-  window.setInterval(preloadingRemove, 1500);
-}
+    //Denne forEach funktion fjerne "active" classen fra paragraph elementerne, men tilføjer en ny "fade" class.
+    //(idx + 1) * 50) beskriver at funktionen tager paragraphens index i rækkefølge, og derefter plusser med 1 hvert 50 millisekunder.
+    //Dette gør at hvert paragraph element først mister "active" classen og derefter bliver givet "fade" classen med 50 millisekunders mellemrum
+    //Dette får paragraphelementerne til at fade ud igen.
+    setTimeout(() => {
+      preloadParagraph.forEach((p, idx) => {
+        setTimeout(() => {
+          p.classList.remove("active");
+          p.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+      //Til sidst er der tilføjet en "total duration" på den øverste setTimeout på 2000 millisekunder (2 sekunder), da animationen på teksten skal køre i 2 sekunder.
+    }, 2000);
 
-window.onload = preloader;
+    //preload.style.top = "100vh" fortæller selve baggrundselementet at kører ned fra skærmen. Hvis man ville have den kørte op i stedet, havde man skrevet "-100vh".
+    setTimeout(() => {
+      preload.style.top = "100vh";
+      //Baggrundsanimationen er sat til at varer 2200 millisekunder (2.2 sekunder).
+    }, 2200);
+  });
+});
 
 //Sticky header til navbar
 const header = document.querySelector("header");
